b0b14b6143a0890e71eeec0c5de9ffec6322d1f9
Berden,Cédric C.P.J. <c.berden@student.fontys.nl>
2025-03-12 11:02:18 +0100

    Add project files.

---
+++ UserManagement/Controllers/UserServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Controllers
+{
+    [ApiController]
+    [Route("api/[user]")]
+    public class UserServiceController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new List<string> { "User1", "User2" };
+            return Ok(users);
+        }
+    }
+}
